5b2fda09d5e1dbf44f995bb9ddb1903f3dbc471a
maen <maenkh@yahoo.com>
2023-02-16 18:39:03 +0300

    show the child list

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,21 @@ class PeopleController < ApplicationController
 
     def show
         @person = Person.find(params[:id])
+        @child = Person.where("parent = '" + @person.name + "'")    
     end
+
+    def edit
+        @person = Person.find(params[:id])        
+     end
+  
+     def update
+        @person= Person.find(params[:id])
+        if @person.update(person_params)
+            redirect_to person_path
+        else
+            render :edit
+        end
+     end
 
     def new
         @person = Person.new
@@ -14,8 +28,18 @@ class PeopleController < ApplicationController
 
     def create 
         @person = Person.create(person_params)
-         @person.save       
-         redirect_to @person
+        if @person.save    
+            redirect_to @person
+            flash[:notice] = "Record successfully created"   
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @person = Person.find(params[:id])
+        @person.destroy
+        redirect_to people_path
     end
 
     private def person_params
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+    validates :name, presence: true, length: {  minimum: 4 }
+    validates :parent, presence: true, length: {  minimum: 4 }
+
+    Person.create(name: nil).valid?
 end
